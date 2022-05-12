61ce4b3ce691ccb3f8344db5bc1ec4a59e3c8f31
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2022-05-12 14:25:33 +0100

    Update Explore_MitoEM.m

--- CODE/Explore_MitoEM.m
+++ CODE/Explore_MitoEM.m
@@ -1,9 +1,18 @@
 
+clear all
+%%
 baseDir = 'C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\MitoEM\';
+dataDir = strcat(baseDir,filesep,'Data');
+%%
+dir0 = dir(strcat(dataDir,filesep,'EM*'));
+dir01 = dir(strcat(dataDir,filesep,dir0(1).name,filesep,'m*'));
+dir02 = dir(strcat(dataDir,filesep,dir0(2).name,filesep,'m*'));
+dirH_train =  dir(strcat(dataDir,filesep,'EM30-H*',filesep,'*train*',filesep,'*.tif'));
+dirH_val   =  dir(strcat(dataDir,filesep,'EM30-H*',filesep,'*val*',filesep,'*.tif'));
+dirR_train =  dir(strcat(dataDir,filesep,'EM30-R*',filesep,'*train*',filesep,'*.tif'));
+dirR_val   =  dir(strcat(dataDir,filesep,'EM30-R*',filesep,'*val*',filesep,'*.tif'));
 
 %%
-dir0 = dir(strcat(baseDir,filesep,'Data',filesep,'EM*'));
-dirH_train =  dir(strcat(baseDir,filesep,'Data',filesep,'EM30-H*',filesep,'*train*',filesep,'*.tif'));
-dirH_val   =  dir(strcat(baseDir,filesep,'Data',filesep,'EM30-H*',filesep,'*val*',filesep,'*.tif'));
-dirR_train =  dir(strcat(baseDir,filesep,'Data',filesep,'EM30-R*',filesep,'*train*',filesep,'*.tif'));
-dirR_val   =  dir(strcat(baseDir,filesep,'Data',filesep,'EM30-R*',filesep,'*val*',filesep,'*.tif'));
+k = 1;
+currSlide = imread(strcat(dataDir,filesep,'EM30-R*',filesep,dirR_val(k).name));
+imagesc(currSlide)
