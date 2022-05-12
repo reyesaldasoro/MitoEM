46a97bdc0c3c6bd5f5e8ac535f1889804f4bbbd5
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2022-05-12 14:55:18 +0100

    Update Explore_MitoEM.m

--- CODE/Explore_MitoEM.m
+++ CODE/Explore_MitoEM.m
@@ -7,12 +7,22 @@ dataDir = strcat(baseDir,filesep,'Data');
 dir0 = dir(strcat(dataDir,filesep,'EM*'));
 dir01 = dir(strcat(dataDir,filesep,dir0(1).name,filesep,'m*'));
 dir02 = dir(strcat(dataDir,filesep,dir0(2).name,filesep,'m*'));
+
+dir011 = dir(strcat(dataDir,filesep,dir0(1).name,filesep,dir01(1).name,filesep,'*.tif'));
+dir012 = dir(strcat(dataDir,filesep,dir0(1).name,filesep,dir01(2).name,filesep,'*.tif'));
+dir021 = dir(strcat(dataDir,filesep,dir0(2).name,filesep,dir01(1).name,filesep,'*.tif'));
+dir022 = dir(strcat(dataDir,filesep,dir0(2).name,filesep,dir01(2).name,filesep,'*.tif'));
+
 dirH_train =  dir(strcat(dataDir,filesep,'EM30-H*',filesep,'*train*',filesep,'*.tif'));
 dirH_val   =  dir(strcat(dataDir,filesep,'EM30-H*',filesep,'*val*',filesep,'*.tif'));
 dirR_train =  dir(strcat(dataDir,filesep,'EM30-R*',filesep,'*train*',filesep,'*.tif'));
 dirR_val   =  dir(strcat(dataDir,filesep,'EM30-R*',filesep,'*val*',filesep,'*.tif'));
 
 %%
-k = 1;
-currSlide = imread(strcat(dataDir,filesep,'EM30-R*',filesep,dirR_val(k).name));
-imagesc(currSlide)
+clear EMImages;
+EMImages(4096,4096,numel(dirH_val)) = 0;
+
+for k=1:numel(dirH_val)
+    EMImages(:,:,k) = imread(strcat(dataDir,filesep,dir0(1).name,filesep,dir01(1).name,filesep,dir011(k).name));
+    disp(k)
+end
